cd2d496179df481c0b812ed0e541bc38e4315e2b
tspeu <tenshotseuropa@gmail.com>
2019-01-08 21:16:54 +0000

    fix

--- api/controller/matchingController.js
+++ api/controller/matchingController.js
@@ -17,16 +17,23 @@ class MatchingController {
     }
     
 
-    /* @PARAMS INPUT
-    ** {workers[] shift []}     
-    */
+    /**
+     * Tries to match an workers with shifts 
+     * @params req, res
+     * @return Lis<Match> entity
+     */
     matchingWorker(req, res) {
     try {
 
         //input 
+
+        // let data = [];
+        // let workersList =[];
+        // let shiftList = [];
         let data = req.body;
         let workersList = req.body.workers;
         let shiftList = req.body.shifts;
+    
 
         //output
         this.matchingList = [];
@@ -70,7 +77,15 @@ class MatchingController {
             // let sortedWorkers = _.sortBy(workersList, ['availability.length', 'payrate']);
         // let sortedWorkers = _.sortBy(workersList, ['availability.length']);//, 'payrate'
         let sortedWorkers = workersList.sort((a, b) => a.availability.length > b.availability.length && a.payrate < b.payrate);
-    
+        this.unEmployedList  = []; 
+        this.employedList  = [];
+
+        /*           **/
+        this.addEmployedList(sortedWorkers,this.unEmployedList  ,this.employedList );
+            console.log('this.unEmployedList------->', this.unEmployedList);
+            console.log('this.employedList------->', this.employedList);
+        /*         */
+
         let countID = 1; 
 
         this.addFLagWorker(sortedWorkers);
@@ -80,7 +95,8 @@ class MatchingController {
             let day = shift.day;
                                 
         //ITERAR TRUE WORKER
-            for (let worker of sortedWorkers) {
+        // for (let worker of sortedWorkers) {
+            for (let worker of this.unEmployedList) {
 
             //check if exists a worker with a day avaible with the day shift
             let index = worker.availability.findIndex(k => k== day);
@@ -105,18 +121,57 @@ class MatchingController {
                 
                 //SORT LIST AGAIN->
                 sortedWorkers = _.sortBy(sortedWorkers, ['availability.length', 'payrate']);
+                        /*           **/
+                this.addEmployedList(sortedWorkers,this.unEmployedList  ,this.employedList );
+                console.log('this.unEmployedList------->', this.unEmployedList);
+                console.log('this.employedList------->', this.employedList);
+    /*         */
                 
                 break;
-            
             }
             
+        }
+
+        for (let worker of this.employedList) {
+
+            //check if exists a worker with a day avaible with the day shift
+            let index = worker.availability.findIndex(k => k== day);
+            
+            if (index !== -1 ) {
+                availableWorkerFound = true;
+                
+                //TODO AGREGAR FLAG DE YA TIENE UN TURNO->
+                //worker.assignedshift = true;
+                worker.hasmoreavailables = (worker.availability.length > 0) ? true: false;
+
+                //eliminated the day for the actual worker
+                worker.availability.splice(index, 1);
+                this.deleteShiftsTaken(day, sortedWorkers);
+
+                this.setMatchingModel(countID,  shift.id, worker.id, day, worker.payrate);
+
+                this.addMatch(this.matchingModel, this.matchingList);
+
+                this.msmOut ="";
+                countID ++;
+                
+                //SORT LIST AGAIN->
+                sortedWorkers = _.sortBy(sortedWorkers, ['availability.length', 'payrate']);
+                        /*           **/
+                this.addEmployedList(sortedWorkers,this.unEmployedList  ,this.employedList );
+                console.log('this.unEmployedList------->', this.unEmployedList);
+                console.log('this.employedList------->', this.employedList);
+    /*         */
+                
+                break;
+            }   
         }
         
         if (availableWorkerFound === false) {
             this.msmErr = "No optimal solution found";
             this.msmOut = `There are no workers available for the required ${shiftsWord}`;
             allShiftsTaken = false;
-            break;
+            //break;
         }
         
         allShiftsTaken = true;    
@@ -179,6 +234,23 @@ class MatchingController {
             worker.hasmoreavailables = false;
             worker.numshift = worker.availability.length;
             worker.sameday = 0;
+        }
+    }
+
+    //DIVIDE FIRST TIME ALL ARE UNEMPLOYED
+    addUnemployeList(inputlist, unemployedList) {
+        for (let unemployed of inputlist) {
+            if (unemployed.assignedshift === false) {
+
+                unemployedList.plush(unemployed);
+            }
+        }
+    }
+
+    addEmployedList(inputList, unemployedList, employedList) {
+        for (let employed of inputList) {
+
+            (employed.assignedshift === true)? employedList.push(employed) : unemployedList.push(employed);
         }
     }
     
